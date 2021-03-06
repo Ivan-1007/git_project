6ec1e176d543240c15feecff802ddd012b6aeb6f
Ivan <ikisel1007@gmail.com>
2021-03-06 15:07:39 +0300

    snowfall task 1 solved

--- 2_working_with_classes/2_snowfall.py
+++ 2_working_with_classes/2_snowfall.py
@@ -7,22 +7,55 @@ import simple_draw as sd
 
 
 class Snowflake:
-    pass
+    fallen_flakes = 0
 
-    # TODO здесь ваш код
+    def __init__(self, color=sd.COLOR_WHITE):
+        self.x = sd.randint(40, sd.resolution[0] - 40)
+        self.y = 610
+        self.len = sd.randint(15, 35)
+        self.start_rays = sd.randint(5, 10) / 10
+        self.len_rays = sd.randint(5, 10) / 10
+        self.angle_rays = sd.randint(30, 160)
+        self.color = color
+
+    def move(self):
+        step_x = sd.randint(-15, 15)
+        self.x = self.x + step_x
+        self.y = self.y - sd.randint(5, 20)
+
+    def draw(self, color='default'):
+        if color == 'default':
+            color = self.color
+        start_point = sd.get_point(self.x, self.y)
+        sd.snowflake(center=start_point, length=self.len, color=color, factor_a=self.start_rays,
+                     factor_b=self.len_rays, factor_c=self.angle_rays)
+
+    def clear_previous_picture(self):
+        self.draw(color=sd.background_color)
+
+    def can_fall(self):
+        return self.y > -15
+
+    def create_new_center(self):
+        self.x = sd.randint(40, 560)
+        self.y = 610
 
 
-flake = Snowflake()
+flake = Snowflake(color=sd.COLOR_CYAN)
+
+print(flake.color)
 
 while True:
     flake.clear_previous_picture()
     flake.move()
     flake.draw()
     if not flake.can_fall():
-        break
+        flake.clear_previous_picture()
+        flake.create_new_center()
     sd.sleep(0.1)
     if sd.user_want_exit():
         break
+
 
 # шаг 2: создать снегопад - список объектов Снежинка в отдельном списке, обработку примерно так:
 # flakes = get_flakes(count=N)  # создать список снежинок
